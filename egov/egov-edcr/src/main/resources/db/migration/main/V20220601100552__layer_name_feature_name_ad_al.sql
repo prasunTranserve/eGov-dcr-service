5de585aadbf56beba361cd23539b88c29531e747
Md Reza Khan <rezakhan9494@gmail.com>
2022-06-01 07:33:32 +0000

    seq updated

--- egov/egov-edcr/src/main/resources/db/migration/main/V20220601100552__layer_name_feature_name_ad_al.sql
+++ egov/egov-edcr/src/main/resources/db/migration/main/V20220601100552__layer_name_feature_name_ad_al.sql
@@ -23,27 +23,27 @@ WHERE NOT EXISTS ( SELECT "key" FROM state.egdcr_layername WHERE "key" = 'LAYER_
 -- Feature AffectedLandArea
 
 INSERT INTO generic.egdcr_sub_feature_colorcode(id, feature, subfeature, colorcode,ordernumber) 
-SELECT nextval('seq_egdcr_sub_feature_colorcode'), 'AffectedLandArea', 'CDP road widening', 1, 1
+SELECT nextval('generic.seq_egdcr_sub_feature_colorcode'), 'AffectedLandArea', 'CDP road widening', 1, 1
 WHERE NOT EXISTS ( SELECT subfeature FROM generic.egdcr_sub_feature_colorcode WHERE feature = 'AffectedLandArea' AND subfeature = 'CDP road widening');
 
 INSERT INTO generic.egdcr_sub_feature_colorcode(id, feature, subfeature, colorcode,ordernumber) 
-SELECT nextval('seq_egdcr_sub_feature_colorcode'), 'AffectedLandArea', 'CDP drain widening', 2, 1
+SELECT nextval('generic.seq_egdcr_sub_feature_colorcode'), 'AffectedLandArea', 'CDP drain widening', 2, 1
 WHERE NOT EXISTS ( SELECT subfeature FROM generic.egdcr_sub_feature_colorcode WHERE feature = 'AffectedLandArea' AND subfeature = 'CDP drain widening');
 
 INSERT INTO generic.egdcr_sub_feature_colorcode(id, feature, subfeature, colorcode,ordernumber) 
-SELECT nextval('seq_egdcr_sub_feature_colorcode'), 'AffectedLandArea', 'Proposed road', 3, 1
+SELECT nextval('generic.seq_egdcr_sub_feature_colorcode'), 'AffectedLandArea', 'Proposed road', 3, 1
 WHERE NOT EXISTS ( SELECT subfeature FROM generic.egdcr_sub_feature_colorcode WHERE feature = 'AffectedLandArea' AND subfeature = 'Proposed road');
 
 INSERT INTO generic.egdcr_sub_feature_colorcode(id, feature, subfeature, colorcode,ordernumber) 
-SELECT nextval('seq_egdcr_sub_feature_colorcode'), 'AffectedLandArea', 'CDP proposed road', 4, 1
+SELECT nextval('generic.seq_egdcr_sub_feature_colorcode'), 'AffectedLandArea', 'CDP proposed road', 4, 1
 WHERE NOT EXISTS ( SELECT subfeature FROM generic.egdcr_sub_feature_colorcode WHERE feature = 'AffectedLandArea' AND subfeature = 'CDP proposed road');
 
 INSERT INTO generic.egdcr_sub_feature_colorcode(id, feature, subfeature, colorcode,ordernumber) 
-SELECT nextval('seq_egdcr_sub_feature_colorcode'), 'AffectedLandArea', 'Road widening', 5, 1
+SELECT nextval('generic.seq_egdcr_sub_feature_colorcode'), 'AffectedLandArea', 'Road widening', 5, 1
 WHERE NOT EXISTS ( SELECT subfeature FROM generic.egdcr_sub_feature_colorcode WHERE feature = 'AffectedLandArea' AND subfeature = 'Road widening');
 
 INSERT INTO generic.egdcr_sub_feature_colorcode(id, feature, subfeature, colorcode,ordernumber) 
-SELECT nextval('seq_egdcr_sub_feature_colorcode'), 'AffectedLandArea', 'Restricted area', 6, 1
+SELECT nextval('generic.seq_egdcr_sub_feature_colorcode'), 'AffectedLandArea', 'Restricted area', 6, 1
 WHERE NOT EXISTS ( SELECT subfeature FROM generic.egdcr_sub_feature_colorcode WHERE feature = 'AffectedLandArea' AND subfeature = 'Restricted area');
 
 
